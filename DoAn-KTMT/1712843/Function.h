dc82e85fb3a9baa4dfb70846cf94f03baae3b4cd
averagedo <37841274+averagedo@users.noreply.github.com>
2019-03-21 15:03:05 +0700

    fix

--- DoAn-KTMT/1712843/Function.h
+++ DoAn-KTMT/1712843/Function.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 #include<string>
 
@@ -61,7 +60,7 @@ int StrSgInt(string a);
 class QFloat
 {
 private:
-	 int qFloat[4];
+	int qFloat[4];
 public:
 	QFloat();
 	void setQFloat(string a);
@@ -70,77 +69,10 @@ public:
 	string BitSauPhay(string a);
 	string BitTruocPhay(string a);
 	string DecToBin(string a);
-	void ScanQFloat(string a,int n);
+	void ScanQFloat(string a, int n);
 
 	string SoPhay(string a);
 	string BinToDec(string a);
 
 	string PrintQFloat(int n);
-=======
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-class QInt
-{
-private:
-	int qInt[4];
-public:
-	QInt();
-	string getQInt();
-	void setQInt(string a);
-	
-	void XuatStrBit(string a);
-	
-
-	void ScanQInt(string a, int n);
-	string PrinQInt(int n);
-
-	string DecToBin(string a);
-
-	string BinToDec(string a);
-
-	string BinToHex(string a);
-
-	string HvsB(char a);
-	string HexToBin(string a);
-
-	QInt operator+(QInt a);
-	QInt operator-(QInt a);
-	int DemBit(string a);
-	QInt operator*(QInt a);
-	QInt operator/(QInt a);
-	
-};
-
-
-string CongBit(string BitA, string BitB); //ca hai deu co 128 bit;
-string BuHai(string a);	//Bieu dien so am
-string CongHaiString(string a, string b);	//khong cung kich thuoc, so duog
-string Mu(int k,int n);	//a^n
-string ChiaHai(string a);
-string Nhan(string a, int n);
-string IntSgString(int a);
-int StrSgInt(string a);
-
-class QFloat
-{
-private:
-	 int qFloat[4];
-public:
-	QFloat();
-	void setQFloat(string a);
-	string getQFloat();
-
-	string BitSauPhay(string a);
-	string BitTruocPhay(string a);
-	string DecToBin(string a);
-	void ScanQFloat(string a,int n);
-
-	string SoPhay(string a);
-	string BinToDec(string a);
-
-	string PrintQFloat(int n);
->>>>>>> 0ed505653179250343810a0c812d843f4d3daf5e
 };
